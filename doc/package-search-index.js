913cbcdb5d124f6e6079a62c1c900f6bd590bf44
Edward Esposito <109972976+EdwardEsposito1@users.noreply.github.com>
2023-05-02 11:46:54 -0400

    Add files via upload

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Chess66"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"Chess66","l":"chess"}];updateSearchResults();
